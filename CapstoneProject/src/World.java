548a3ff64128f4e78320ff8973e21c3239dc8b1e
Jerry Chu <mlgerrydoge@gmail.com>
2022-05-03 13:31:21 -0700

    added square movement

--- CapstoneProject/src/World.java
+++ CapstoneProject/src/World.java
@@ -12,6 +12,11 @@ public class World extends PApplet{
 	public int screenHeight = maxScreenRow * tileSize;
 	float x, y;
 	
+
+    boolean north;
+	boolean south;
+	boolean west;
+	boolean east;
 	
 	public World() {
 		
@@ -28,16 +33,43 @@ public class World extends PApplet{
 		background(255);  
 		fill(255);
 		textAlign(CENTER);
-		
-		fill(0);
-		rect(x, y, 100, 100);
-		fill(255);
-		
+			 
+			  moveObject();
+			//  confineToEdges();
+			  fill(0);
+			  rect(x, y, 100, 100);
+	}
+			 
+	public	void keyPressed() {
+			  final int k = keyCode;
+			  setDirection(k, true);
+			}
+			 
+	public void keyReleased() {
+			  setDirection(keyCode, false);
+			}
+			 
+	public void setDirection(int k, boolean decision) {
+			  if      (k == 'w'    || k == 'W')   north = decision;
+			  else if (k == 's'  || k == 'S')   south = decision;
+			  else if (k == 'a'  || k == 'A')   west  = decision;
+			  else if (k == 'd' || k == 'D')   east  = decision;
+			}
+			 
+	public void moveObject() {
+			  x += (east?  20 : 0) - (west?  20 : 0);
+			  y += (south? 20 : 0) - (north? 20 : 0);
+			}
+//			 
+//			static final void confineToEdges() {
+//			  x = constrain(x, ww, gw);
+//			  y = constrain(y, hh, gh);
+//			}
+	
 		
 
 		
-		
-	}
+	
 	
 	
 	public void mousePressed() {
@@ -48,22 +80,8 @@ public class World extends PApplet{
 	public void mouseDragged() {
 		
 	}
+
 	
-	public void keyPressed() {
-		if (key == 'w' || key == 'W') {
-			y-=10;
-		}
-		if (key == 'a' || key == 'A') {
-			x-=10;
-		}
-		if (key == 's' || key == 'S') {
-			y+=10;			
-		}
-		if (key == 'd' || key == 'D') {
-			x+=10;
-		}
-	
-	}
 	
 
 }
