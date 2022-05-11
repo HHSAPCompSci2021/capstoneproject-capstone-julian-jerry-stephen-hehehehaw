717f58130fee82749f5f5f94c2699175a7066572
Stephen Tan <88912136+stan204@users.noreply.github.com>
2022-05-11 11:50:23 -0700

    Added button to MainMenu

--- CapstoneProject/src/Screens/FirstScreen.java
+++ CapstoneProject/src/Screens/FirstScreen.java
@@ -9,28 +9,38 @@ public class FirstScreen extends Screen {
 
 	private MainMenu surface;
 	
-	private Rectangle button;
-
+	private Rectangle startButton;
+	private Rectangle instButton;
+	
 	public FirstScreen(MainMenu surface) {
 		super(800,600);
 		this.surface = surface;
 
-		button = new Rectangle(800/2-100,600/2-50,200,100);
+//		button = new Rectangle(800/2-100,600/2-50,200,100);
+		startButton = new Rectangle(800/2-100,600/3,200,100);
+		instButton = new Rectangle(800/2-100, 600/3*2,200,100);
 	}
 
 	// add one extra button for the Instructions screen
 	// connect the frame size to that in Main and the first button should lead to the World class
 	public void draw() {
+		System.out.println("You are on the 1st Screen");
 		
+		// start button
 		surface.background(0);
 		
-		surface.rect(button.x, button.y, button.width, button.height, 10, 10, 10, 10);
-		surface.fill(0);
-		String str = "Click me!";
+		surface.rect(startButton.x, startButton.y, startButton.width, startButton.height, 10, 10, 10, 10);
+		String str = "Start!";
 		float w = surface.textWidth(str);
-		surface.text(str, button.x+button.width/2-w/2, button.y+button.height/2);
+		surface.text(str, startButton.x+startButton.width/2-w/2, startButton.y+startButton.height/2);
 		
-		System.out.println("You are on the 1st Screen");
+		
+		// instructions button
+		
+		surface.rect(instButton.x, instButton.y, instButton.width, instButton.height, 10, 10, 10, 10);
+		String str2 = "Instructions";
+		float w2 = surface.textWidth(str);
+		surface.text(str2, instButton.x+instButton.width/2-w2/2, instButton.y+instButton.height/2);
 	}
 
 
@@ -38,8 +48,10 @@ public class FirstScreen extends Screen {
 	
 	public void mousePressed() {
 		Point p = surface.actualCoordinatesToAssumed(new Point(surface.mouseX,surface.mouseY));
-		if (button.contains(p))
+		if (startButton.contains(p))
 			surface.switchScreen(ScreenSwitcher.GAME_SCREEN);
+		else if (instButton.contains(p))
+			surface.switchScreen(ScreenSwitcher.INSTRUCTION_SCREEN);
 	}
 	
 
--- CapstoneProject/src/Screens/ScreenSwitcher.java
+++ CapstoneProject/src/Screens/ScreenSwitcher.java
@@ -4,6 +4,7 @@ package Screens;
 public interface ScreenSwitcher {
 	public static final int MENU_SCREEN = 0;
 	public static final int GAME_SCREEN = 1;
+	public static final int INSTRUCTION_SCREEN = 2;
 	
 	public void switchScreen(int i);
 }
