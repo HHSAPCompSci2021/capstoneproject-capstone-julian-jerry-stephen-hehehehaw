d6cb7bd78bcf1c634850984c26a4533a0ece40a5
Stephen Tan <88912136+stan204@users.noreply.github.com>
2022-05-13 10:35:28 -0700

    Beta Build

---
+++ CapstoneProject/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Instructions"},{"l":"Players"},{"l":"PowerUps"},{"l":"Screens"},{"l":"Tiles"},{"l":"Weapons"}];updateSearchResults();
